22dd7de9e641dda6291b5f116d8bff46110863db
ILIA <brakerilya@yandex.ru>
2025-02-03 13:06:53 +0300

    change script

--- src/script.js
+++ src/script.js
@@ -1,21 +1,29 @@
 const detailedImage = document.querySelector(".detail_image");
-const detailedTitle=document.querySelector(".detail--item_title");
+const detailedTitle=document.querySelector(".container--detail_title");
 const boxesElements = document.querySelectorAll(".gallery--item");
-let boxAnimationElement=document.getElementById("box_animate")
 function setDetails(boxElement){
-    detailedImage.src=boxElement.getAttribute("data-alt-text");
-    detailedTitle.innerHTML=boxElement.getAttribute("data-detailed-text");
+detailedImage.src=boxElement.getAttribute("data-alt-text");
+detailedTitle.innerHTML=boxElement.getAttribute("data-detailed-text");
 }
 for(let i= 0;i< boxesElements.length;i++ ){
     boxesElements[i].addEventListener("click",function(){
         setDetails(boxesElements[i]);
-    
-    boxAnimationElement.classList.toggle("animation")
+        animate()
     
  
     })
 }
-boxAnimationElement.classList.add("animation")
+function animate(){
+    detailedImage.classList.remove("animation")
+    detailedTitle.classList.remove("animation")
+    setTimeout(function () {
+        detailedImage.classList.add("animation");
+        detailedTitle.classList.add("animation");
+      }, 0);
+
+}
+detailedImage.classList.add("animation");
+detailedTitle.classList.add("animation");
 
    
 
